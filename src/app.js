2563eac8d5f637d3fddd9e097d76bb6dbc837d3b
Saikrishna1819-lang <krishnasai69219@gmail.com>
2025-05-21 14:45:47 +0530

    multiple router handlers

--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
 const express=require("express");
 const  app=express();
 
-app.use("/",(req,res)=>{
-    res.send("/ route is working")
-})
-app.post("/home",(req,res)=>{
-    res.send("data sucessfully added to the database");
-})
-
-app.get("/home",(req,res)=>{
-
-    res.send("Data received sucessfully");
+app.use("/home",(req,res,next)=>{
+  
+  
+       next();
+   
+},(req,res)=>{
+    res.send("2nd response");
 })
 
 
