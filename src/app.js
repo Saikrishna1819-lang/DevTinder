6ffe1d4cf00d0d2bedbe8c745ec0dc0e24cde013
Saikrishna1819-lang <krishnasai69219@gmail.com>
2025-05-21 11:58:11 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 const express=require("express");
 const  app=express();
-app.use("/home",function(req,res){
-res.send("sai krishna from the server  ");
+
+app.use("/",(req,res)=>{
+    res.send("/ route is working")
 })
+app.post("/home",(req,res)=>{
+    res.send("data sucessfully added to the database");
+})
+
+app.get("/home",(req,res)=>{
+
+    res.send("Data received sucessfully");
+})
+
+
+
+
+
+
+
+
+
 app.listen(3000);
