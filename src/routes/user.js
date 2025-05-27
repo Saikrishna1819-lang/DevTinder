45fad7a4259ceb8fb3867246bad0e98af7d02bc2
Saikrishna1819-lang <krishnasai69219@gmail.com>
2025-05-27 17:38:37 +0530

    feed  api is created and pagination is added

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,10 +3,11 @@ const { userAuth } = require("../middlewares/auth");
 const userRouter=express.Router();
 const ConnectionRequest=require("../models/connectionRequest")
 const USER_SAFE_DATA="firstName lastName about age skills ";
+const User=require("../models/user");
 
 userRouter.get("/user/received",userAuth,async(req,res)=>{
     try{
-        console.log("sai krishna")
+       
 
         const loggedInUser=req.user;
         const connectionRequests=await ConnectionRequest.find({
@@ -35,7 +36,7 @@ userRouter.get("/user/connections",userAuth,async(req,res)=>{
         ]
    
     }).populate("fromUserId",USER_SAFE_DATA).populate("toUserId",USER_SAFE_DATA);
-     console.log(connectionRequests);
+    
     const data=connectionRequests.map((row)=>{
         if(row.fromUserId._id.toString()===loggedInUser._id.toString())
         {
@@ -49,5 +50,45 @@ userRouter.get("/user/connections",userAuth,async(req,res)=>{
         res.status(404).send("ERROR:"+err.message);
     }
 
+})
+
+userRouter.get("/feed",userAuth,async(req,res)=>{
+    try{
+
+        const loggedInUser=req.user;
+        const page=parseInt(req.query.page)||1;
+        let limit=parseInt(req.query.limit)||10;
+        const skip=(page-1)*limit;
+        limit=limit>50? 50:limit;
+        const connectionRequests= await ConnectionRequest.find({
+            $or:[
+                {fromUserId:loggedInUser._id},{toUserId:loggedInUser._id}
+            ]
+        }).select("fromUserId toUserId");
+
+        const hideUserFromFeed=new Set();
+        connectionRequests.forEach((req)=>{
+            hideUserFromFeed.add(req.fromUserId.toString());
+            hideUserFromFeed.add(req.toUserId.toString());
+        })
+        
+
+        const users=await User.find({
+            $and:[
+                {_id:{$nin:Array.from(hideUserFromFeed)}},
+                {_id:{$nin:loggedInUser._id}}
+            ]
+            
+            
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+        
+        res.json({users})
+
+
+    }catch(err)
+    {
+        res.status(500).send("ERROR:"+err.message);
+
+    }
 })
 module.exports=userRouter;
